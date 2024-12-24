4317da31a8d16f51dcc96d8f399ceb5160e65dcf
vellun <moonpassiflora@gmail.com>
2024-12-25 02:33:53 +0300

    [feat] add players

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,44 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Board } from "./models/Board";
 import BoardComponent from "./components/BoardComponent";
+import { Player } from "./models/Player";
+import { Colors } from "./models/Colors";
 
 function App() {
   const [board, setBoard] = useState(new Board());
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
     restart();
+    setCurrentPlayer(whitePlayer);
   }, []);
 
   function restart() {
     const newBoard = new Board();
     newBoard.initCells();
-    newBoard.addFigures()
+    newBoard.addFigures();
     setBoard(newBoard);
   }
-  console.log(board)
+
+  function swapPlayer() {
+    setCurrentPlayer(
+      currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer
+    );
+  }
 
   return (
-    <div className="App">
-      <BoardComponent board={board} setBoard={setBoard} />
+    <div>
+      <h3>Сейчас ходят {currentPlayer?.color} котики</h3>
+      <div className="App">
+        <BoardComponent
+          board={board}
+          setBoard={setBoard}
+          currentPlayer={currentPlayer}
+          swapPlayer={swapPlayer}
+        />
+      </div>
     </div>
   );
 }
